2d60ee3de3a8569d7b7ae57e2714ae26dec57872
ChangxingJiang <1278729001@qq.com>
2020-10-23 10:37:42 +0800

    release v0.0.14

--- Utils4R/__init__.py
+++ Utils4R/__init__.py
@@ -5,5 +5,6 @@ from . import static
 
 from .basic import console
 from .basic import get_scope_random as random
+from .fetch import fetch_number
 from .request import do_request
 from .request import try_request
