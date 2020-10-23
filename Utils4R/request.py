e3027732cc0a209f54a24376ddf77ef6ffad7d38
ChangxingJiang <1278729001@qq.com>
2020-10-23 21:08:11 +0800

    release v0.0.16

--- Utils4R/request.py
+++ Utils4R/request.py
@@ -3,7 +3,7 @@ import time
 import requests
 
 
-def do_request(url, headers, proxies=None):
+def do_request(url, headers=None, proxies=None):
     """执行HTTP请求：如果请求成功则返回请求结果；如果请求失败则返回None"""
     try:
         if response := requests.get(url=url, headers=headers, proxies=proxies):
@@ -12,7 +12,7 @@ def do_request(url, headers, proxies=None):
         return None
 
 
-def try_request(url, headers, proxies=None, times=3, interval=10):
+def try_request(url, headers=None, proxies=None, times=3, interval=10):
     """尝试执行HTTP请求：尝试请求times次，每次请求之间间隔interval秒，如果最终请求失败则返回None"""
     for _ in range(times):
         if response := do_request(url=url, headers=headers, proxies=proxies):
